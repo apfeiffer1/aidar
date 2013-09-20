5fbaaffe6ae3ebba9168a8522b32810e6859d811
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2013-09-20 10:29:30 +0200

    # This is a combination of 4 commits. # The first commit's message is: fixed wrong comparison
    # This is the 2nd commit message:
    
    updated
    
    # This is the 3rd commit message:
    
    added more info
    
    # This is the 4th commit message:
    
    updated Namespace

--- inst/tests/test_tup.r
+++ inst/tests/test_tup.r
@@ -12,9 +12,9 @@ test_that("reading a tuple from file", {
 	expect_equal( length(t100),    4 )  # nCol
 	expect_equal( length(t100$lin), 1000 )  # nRow	
 	
-	expect_equal( mean(t100$sin)  ,  0.001628846, tolerance = 1.E-6, scale = mean(h1$entries) )
-	expect_equal( mean(t100$gauss), -0.000790439, tolerance = 1.E-6, scale = mean(h1$entries) )
-	expect_equal( mean(t100$flat) , 24.89807    , tolerance = 1.E-6, scale = mean(h1$entries) )
+	expect_equal( mean(t100$sin)  ,  0.001628846, tolerance = 1.E-6, scale = mean(t100$sin) )
+	expect_equal( mean(t100$gauss), -0.000790439, tolerance = 1.E-6, scale = mean(t100$gauss) )
+	expect_equal( mean(t100$flat) , 24.89807    , tolerance = 1.E-6, scale = mean(t100$flat) )
 	expect_equal( max (t100$lin)  , 999 )
 	
 })
